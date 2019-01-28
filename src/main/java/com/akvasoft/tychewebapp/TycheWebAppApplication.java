84bdeefd162982f4b6b9cb18543523e03b1688b0
dhanushka350 <dhanushka@akvasoft.com>
2019-01-28 10:56:04 +0530

    boot&angular apps

---
+++ src/main/java/com/akvasoft/tychewebapp/TycheWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.akvasoft.tychewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TycheWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TycheWebAppApplication.class, args);
+	}
+
+}
+
