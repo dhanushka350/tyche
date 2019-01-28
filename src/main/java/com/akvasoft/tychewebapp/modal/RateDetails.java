1a47baa8244fd5c83c4d294119262177c1b08805
dhanushka350 <dhanushka@akvasoft.com>
2019-01-28 11:35:07 +0530

    angular package added

---
+++ src/main/java/com/akvasoft/tychewebapp/modal/RateDetails.java
@@ -0,0 +1,90 @@
+package com.akvasoft.tychewebapp.modal;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "T_RATE_DETAILS")
+public class RateDetails {
+    @Id
+    @Column(name = "RATE_DETAILS_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "SYMBOL")
+    private String symbol;
+    @Column(name = "BID")
+    private String bid;
+    @Column(name = "ASK")
+    private String ask;
+    @Column(name = "SHORT_VALUE")
+    private String shortValue;
+    @Column(name = "CHANGE_OPEN_INTEREST")
+    private String changeOpenInterest;
+    @Column(name = "INNER_LINK")
+    private String innerLink;
+    @Column(name = "SCR_DATE")
+    private String date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getBid() {
+        return bid;
+    }
+
+    public void setBid(String bid) {
+        this.bid = bid;
+    }
+
+    public String getAsk() {
+        return ask;
+    }
+
+    public void setAsk(String ask) {
+        this.ask = ask;
+    }
+
+    public String getShortValue() {
+        return shortValue;
+    }
+
+    public void setShortValue(String shortValue) {
+        this.shortValue = shortValue;
+    }
+
+    public String getChangeOpenInterest() {
+        return changeOpenInterest;
+    }
+
+    public void setChangeOpenInterest(String changeOpenInterest) {
+        this.changeOpenInterest = changeOpenInterest;
+    }
+
+    public String getInnerLink() {
+        return innerLink;
+    }
+
+    public void setInnerLink(String innerLink) {
+        this.innerLink = innerLink;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
