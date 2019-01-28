2196b20c0900b0768e3d2e2afa6d928f85503e0d
Chandimal <Schandimal>
2019-01-28 15:42:25 +0530

    UI

--- src/main/angularNew/src/app/app-routing.module.ts
+++ src/main/angularNew/src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {ChartsComponent} from "./charts/charts.component";
+import {LongerTermChartComponent} from "./longer-term-chart/longer-term-chart.component";
+import {DataPullComponent} from "./data-pull/data-pull.component";
+import {DataStreamComponent} from "./data-stream/data-stream.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'charts',
+    component: ChartsComponent
+  },
+  {
+    path: 'longerTermChart',
+    component: LongerTermChartComponent
+  },
+  {
+    path: 'dataPull',
+    component: DataPullComponent
+  },
+  {
+    path: 'dataStream',
+    component: DataStreamComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/main/angularNew/src/app/app.module.ts
+++ src/main/angularNew/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavibarComponent } from './navibar/navibar.component';
+import { ChartsComponent } from './charts/charts.component';
+import { LongerTermChartComponent } from './longer-term-chart/longer-term-chart.component';
+import { DataPullComponent } from './data-pull/data-pull.component';
+import { DataStreamComponent } from './data-stream/data-stream.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavibarComponent,
+    ChartsComponent,
+    LongerTermChartComponent,
+    DataPullComponent,
+    DataStreamComponent
   ],
   imports: [
     BrowserModule,
